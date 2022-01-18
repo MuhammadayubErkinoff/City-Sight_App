b9550a3a94436559573489c5143635f3a4dbc25e
Muhammadayub Erkinoff <muhammadayub2006@gmail.com>
2022-01-18 18:34:04 +0500

    Initial Commit

---
+++ City Sight App/City_Sight_App.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sight_App.swift
+//  City Sight App
+//
+//  Created by Muhammadayubxon on 18/01/22.
+//
+
+import SwiftUI
+
+@main
+struct City_Sight_App: App {
+    var body: some Scene {
+        WindowGroup {
+            LaunchView()
+        }
+    }
+}
---
+++ City Sight App/Views/LaunchView.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchView.swift
+//  City Sight App
+//
+//  Created by Muhammadayubxon on 18/01/22.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
